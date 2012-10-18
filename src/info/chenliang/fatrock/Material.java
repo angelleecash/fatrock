efc99e2272197ad6d3bfca97a467d6ca74dff32a
Liang Chen <chenliangcareer@gmail.com>
2012-10-18 11:54:48 +0800

    fat rock is officially a separate project
    yeah~~~~

---
+++ src/info/chenliang/fatrock/Material.java
@@ -0,0 +1,10 @@
+package info.chenliang.fatrock;
+
+import info.chenliang.ds.Vector3d;
+
+public class Material {
+	public Vector3d emission;
+	public Vector3d ambient;
+	public Vector3d diffuse;
+	public Vector3d specular;
+}
---
+++ src/info/chenliang/fatrock/Texture.java
@@ -0,0 +1,13 @@
+package info.chenliang.fatrock;
+
+import android.graphics.Bitmap;
+
+public class Texture {
+	public Bitmap bitmap;
+
+	public Texture(Bitmap bitmap) {
+		super();
+		this.bitmap = bitmap;
+	}
+	
+}
